933f32b4181ff438d3cd98974eff364e27c17be9
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-09-27 13:50:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+window = Tk()
+
+window.title("Miles to Km Converter")
+window.minsize(width=500, height=300)
+window.config(padx = 40, pady = 40)
+
+def miles_to_km():
+    miles =float(input.get())
+    km = round(miles * 1.609)
+    km_result.config(text=f"{km}")
+
+miles_label = Label(text="Miles")
+miles_label.grid(row=0, column=3)
+
+km_label = Label(text="Km")
+km_label.grid(row=1, column=3)
+
+is_equal_to_label = Label(text="is equal to")
+is_equal_to_label.grid(row=1, column=1)
+
+km_result = Label(text="0")
+km_result.grid(row=1, column=2)
+
+input = Entry(width=10,text="0")
+print(input.get())
+input.grid(row=0, column=2)
+
+
+calculate_button = Button(text="Calculate",command=miles_to_km)
+calculate_button.config(padx = 10, pady = 10)
+calculate_button.grid(row=2, column=2)
+
+
+window.mainloop()
