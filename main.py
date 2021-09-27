b1170d4a923b151d179845845fde7cfeef2fc05b
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-09-27 13:52:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 window = Tk()
 
 window.title("Miles to Km Converter")
-# window.minsize(width=500, height=300)
+window.minsize(width=500, height=300)
 # window.config(padx = 40, pady = 40)
 
 # def miles_to_km():
